b0b30f117c77721ee46f768da1034b2f8fa65d8b
yor_email <daniil.stebnitsky@gmail.com>
2022-08-24 22:53:30 +0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -1,19 +1,25 @@
 package ru.job4j.tracker.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
 @Entity
 @Table(name = "items")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item implements Comparable<Item> {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss");
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private long id;
+    @EqualsAndHashCode.Include
     private String name;
     private String description = "";
     private LocalDateTime created = LocalDateTime.now();
@@ -43,34 +49,6 @@ public class Item implements Comparable<Item> {
         this.description = description;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     @Override
     public String toString() {
         String n = System.lineSeparator();
@@ -79,25 +57,6 @@ public class Item implements Comparable<Item> {
                 name, n,
                 description, n,
                 FORMATTER.format(created));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Item)) {
-            return false;
-        }
-        Item item = (Item) o;
-        return Objects.equals(id, item.id)
-                && name.equals(item.name)
-                && Objects.equals(created.withNano(0), item.created.withNano(0));
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        result *= 31 + Objects.hashCode(id);
-        result *= 31 + created.hashCode();
-        return result;
     }
 
     @Override
