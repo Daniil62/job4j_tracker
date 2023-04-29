57b917de579be0d8d4928977a367dc30cd611491
yor_email <daniil.stebnitsky@gmail.com>
2023-04-29 22:13:35 +0300

    StartUI.java, update_001.sql: refactoring

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,6 +1,6 @@
 create table if not exists items(
 	id serial primary key,
 	name text,
-	created timestamp,
+	created timestamp without time zone,
 	description varchar
 );
--- src/main/java/ru/job4j/tracker/view/StartUI.java
+++ src/main/java/ru/job4j/tracker/view/StartUI.java
@@ -3,21 +3,26 @@ package ru.job4j.tracker.view;
 import ru.job4j.tracker.input.ConsoleInput;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
-import ru.job4j.tracker.logic.HbmTracker;
+import ru.job4j.tracker.logic.SqlTracker;
 import ru.job4j.tracker.logic.Store;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.usecases.*;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class StartUI {
 
     private static void showMenu(List<UserAction> actions) {
         AtomicInteger i = new AtomicInteger();
-        actions.forEach(a -> System.out.println(String.format("%d %s", i.getAndIncrement(), a.name())));
+        actions.forEach(a -> System.out.printf("%d %s%n", i.getAndIncrement(), a.name()));
         System.out.println();
     }
 
@@ -30,20 +35,50 @@ public class StartUI {
         }
     }
 
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+        String value = System.getenv(sysEnv);
+        if (value == null) {
+            value = config.getProperty(key);
+        }
+        return value;
+    }
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = StartUI.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
+    }
+
     public static void main(String[] args) {
-        ArrayList<UserAction> actions = new ArrayList<>();
-        Output output = new ConsoleOutput();
-        actions.add(new CreateAction(output));
-        actions.add(new ShowAllAction(output));
-        actions.add(new ReplaceAction(output));
-        actions.add(new DeleteAction(output));
-        actions.add(new FindByIdAction(output));
-        actions.add(new FindByNameAction(output));
-        actions.add(new ExitAction(output));
-        Input input = new ConsoleInput();
-        Input validate = new ValidateInput(input);
-        Store tracker = new HbmTracker();
-        tracker.init();
-        new StartUI().init(validate, tracker, actions);
+        try (Connection connection = loadConnection()) {
+            Output output = new ConsoleOutput();
+            Input input = new ConsoleInput();
+            Input validate = new ValidateInput(input);
+            Store tracker = new SqlTracker(connection);
+            List<UserAction> actions = List.of(
+                    new CreateAction(output),
+                    new ShowAllAction(output),
+                    new ReplaceAction(output),
+                    new DeleteAction(output),
+                    new FindByIdAction(output),
+                    new FindByNameAction(output),
+                    new ExitAction(output));
+            new StartUI().init(validate, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
